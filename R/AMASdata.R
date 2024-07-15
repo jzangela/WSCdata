220bdb9150fbdcdabb199ab0a96769d147abbcb3
jzangela <jz3101@tc.columbia.edu>
2024-07-15 16:48:34 -0500

    Add description for AMAS dataset

---
+++ R/AMASdata.R
@@ -0,0 +1,26 @@
+#' AMAS Dataset
+#'
+#' This dataset contains math anxiety measurements using the Abbreviated Math Anxiety Scale (AMAS)
+#' on 2,200 Amazon Mechanical Turk workers (Keller et al., ....)
+#' This scale has 9 items representing mathematics anxiety which is a negative emotion associated
+#' to mathematics related activities.
+#'
+#'
+#' @name AMASdata
+#' @docType data
+#' @format amas_wsc is a data frame with 2200 cases (rows) and 9 variables (columns).
+#' The variables are named item1, item2, ... , item9.
+#'
+#' @usage data(AMASdata)
+#'
+#' @source Keller et al., ....
+#'
+#' @references Hopko, D., Mahadevan, R., Bare, R. L., & Hunt, M. K. (2003). The abbreviated math
+#' anxiety scale (AMAS): Construction, validity, and reliability. Assessment, 10 , 78–182.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' summary(amas_wsc)
+
+NULL
